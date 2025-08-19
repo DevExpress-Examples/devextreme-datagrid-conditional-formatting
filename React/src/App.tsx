4b1797d58315a8e447a4b64ef4cb1c5318df63a4
DuckTieCorpMember <danyshbek1996@gmail.com>
2025-08-19 16:40:23 +0500

    Run lint and build

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,16 +1,106 @@
-import { useCallback, useState } from 'react';
-import './App.css';
+import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import Button from 'devextreme-react/button';
+import './App.css';
+import {
+  DataGrid,
+  GroupPanel,
+  Grouping,
+  SortByGroupSummaryInfo,
+  Column,
+  Summary,
+  GroupItem,
+  TotalItem,
+  type DataGridTypes,
+} from 'devextreme-react/data-grid';
+import service from './data.js';
+
+const orders = service.getOrders();
+
+function onCellPrepared(e: DataGridTypes.CellPreparedEvent): void {
+  if (e.rowType === 'data') {
+    if (e.data.OrderDate < new Date(2014, 2, 3)) {
+      e.cellElement.style.color = '#AAAAAA';
+    }
+    if (e.data.SaleAmount > 15000) {
+      if (e.column.dataField === 'OrderNumber') {
+        e.cellElement.style.fontWeight = 'bold';
+      }
+      if (e.column.dataField === 'SaleAmount') {
+        e.cellElement.style.backgroundColor = '#FFBB00';
+        e.cellElement.style.color = '#000000';
+      }
+    }
+  }
+
+  if (e.rowType === 'group') {
+    if (e.row.groupIndex === 0) {
+      e.cellElement.style.backgroundColor = '#BEDFE6';
+    }
+    if (e.row.groupIndex === 1) {
+      e.cellElement.style.backgroundColor = '#C9ECD7';
+    }
+    e.cellElement.style.color = '#000';
+    if (e.cellElement.firstChild && (e.cellElement.firstChild as HTMLElement).style) {
+      (e.cellElement.firstChild as HTMLElement).style.color = '#000';
+    }
+  }
+
+  if (e.rowType === 'groupFooter' && e.column.dataField === 'SaleAmount') {
+    e.cellElement.style.fontStyle = 'italic';
+  }
+}
 
 function App(): JSX.Element {
-  var [count, setCount] = useState<number>(0);
-  const clickHandler = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, [setCount]);
   return (
-    <div className="main">
-      <Button text={`Click count: ${count}`} onClick={clickHandler} />
+    <div className="app">
+      <DataGrid
+        id="gridContainer"
+        dataSource={orders}
+        showBorders={true}
+        onCellPrepared={onCellPrepared}
+      >
+        <GroupPanel visible={true} />
+        <Grouping autoExpandAll={true} />
+        <SortByGroupSummaryInfo summaryItem="count" />
+
+        <Column dataField="Employee" groupIndex={0} />
+        <Column dataField="OrderNumber" caption="Invoice Number" width={130} />
+        <Column dataField="OrderDate" dataType="date" width={160} />
+        <Column dataField="CustomerStoreCity" caption="City" groupIndex={1} />
+        <Column dataField="CustomerStoreState" caption="State" />
+        <Column dataField="SaleAmount" alignment="right" format="currency" sortOrder="desc" />
+
+        <Summary>
+          <GroupItem
+            column="OrderNumber"
+            summaryType="count"
+            displayFormat="{0} orders"
+            alignByColumn={false}
+          />
+          <GroupItem
+            column="SaleAmount"
+            summaryType="max"
+            displayFormat="Max: {0}"
+            valueFormat="currency"
+            alignByColumn={true}
+            showInGroupFooter={false}
+          />
+          <GroupItem
+            column="SaleAmount"
+            summaryType="sum"
+            displayFormat="Sum: {0}"
+            valueFormat="currency"
+            alignByColumn={true}
+            showInGroupFooter={true}
+          />
+          <TotalItem
+            column="SaleAmount"
+            summaryType="sum"
+            displayFormat="Total Sum: {0}"
+            valueFormat="currency"
+          />
+        </Summary>
+      </DataGrid>
     </div>
   );
 }
--- Vue/src/components/HomeContent.vue
+++ Vue/src/components/HomeContent.vue
@@ -1,28 +1,152 @@
-<script setup lang="ts">
-import { computed, ref } from 'vue';
-
-import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import DxButton from 'devextreme-vue/button';
-
-const props = defineProps({
-  text: {
-    type: String,
-    default: 'count',
-  },
-});
-const count = ref(0);
-const buttonText = computed<string>(
-  () => `Click ${props.text}: ${count.value}`
-);
-function clickHandler() {
-  count.value += 1;
-}
-</script>
 <template>
   <div>
-    <DxButton
-      :text="buttonText"
-      @click="clickHandler"
-    />
+    <DxDataGrid
+      id="gridContainer"
+      :data-source="orders"
+      :show-borders="true"
+      @cell-prepared="cellPrepared"
+    >
+
+      <DxGroupPanel :visible="true"/>
+      <DxGrouping :auto-expand-all="true"/>
+      <DxSortByGroupSummaryInfo summary-item="count"/>
+
+      <DxColumn
+        :group-index="0"
+        data-field="Employee"
+      />
+      <DxColumn
+        :width="130"
+        data-field="OrderNumber"
+        caption="Invoice Number"
+      />
+      <DxColumn
+        :width="160"
+        data-field="OrderDate"
+        data-type="date"
+      />
+      <DxColumn
+        :group-index="1"
+        data-field="CustomerStoreCity"
+        caption="City"
+      />
+      <DxColumn
+        data-field="CustomerStoreState"
+        caption="State"
+      />
+      <DxColumn
+        data-field="SaleAmount"
+        alignment="right"
+        format="currency"
+        sort-order="desc"
+      />
+
+      <DxSummary>
+        <DxGroupItem
+          :align-by-column="false"
+          column="OrderNumber"
+          summary-type="count"
+          display-format="{0} orders"
+        />
+        <DxGroupItem
+          :align-by-column="true"
+          :show-in-group-footer="false"
+          column="SaleAmount"
+          summary-type="max"
+          display-format="Max: {0}"
+          value-format="currency"
+        />
+        <DxGroupItem
+          :align-by-column="true"
+          :show-in-group-footer="true"
+          column="SaleAmount"
+          summary-type="sum"
+          display-format="Sum: {0}"
+          value-format="currency"
+        />
+
+        <DxTotalItem
+          column="SaleAmount"
+          summary-type="sum"
+          display-format="Total Sum: {0}"
+          value-format="currency"
+        />
+      </DxSummary>
+    </DxDataGrid>
   </div>
 </template>
+
+<script lang="ts">
+import {
+  DxDataGrid,
+  DxColumn,
+  DxSummary,
+  DxGroupPanel,
+  DxGrouping,
+  DxGroupItem,
+  DxSortByGroupSummaryInfo,
+  DxTotalItem,
+  type DxDataGridTypes
+} from 'devextreme-vue/data-grid';
+import service from './data';
+
+export default {
+  name: 'HomeContent',
+  components: {
+    DxDataGrid,
+    DxColumn,
+    DxSummary,
+    DxGroupPanel,
+    DxGrouping,
+    DxGroupItem,
+    DxSortByGroupSummaryInfo,
+    DxTotalItem
+  },
+  data() {
+    return {
+      orders: service.getOrders()
+    };
+  },
+  methods: {
+    cellPrepared(e: DxDataGridTypes.CellPreparedEvent) {
+      if(e.rowType === 'data') {
+        if(e.data.OrderDate < new Date(2014, 2, 3)) {
+          e.cellElement.style.color = '#AAAAAA';
+        }
+        if(e.data.SaleAmount > 15000) {
+          if(e.column.dataField === 'OrderNumber') {
+            e.cellElement.style.fontWeight = 'bold';
+          }
+          if(e.column.dataField === 'SaleAmount') {
+            e.cellElement.style.backgroundColor = '#FFBB00';
+            e.cellElement.style.color = '#000000';
+          }
+        }
+      }
+
+      if(e.rowType === 'group') {
+        if(e.row.groupIndex === 0) {
+          e.cellElement.style.backgroundColor = '#BEDFE6';
+        }
+        if(e.row.groupIndex === 1) {
+          e.cellElement.style.backgroundColor = '#C9ECD7';
+        }
+
+        e.cellElement.style.color = '#000';
+        if(e.cellElement.firstChild && (e.cellElement.firstChild as HTMLElement).style) {
+          (e.cellElement.firstChild as HTMLElement).style.color = '#000';
+        }
+      }
+
+      if(e.rowType === 'groupFooter' && e.column.dataField === 'SaleAmount') {
+        e.cellElement.style.fontStyle = 'italic';
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
